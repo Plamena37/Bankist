7e538cf37a91c2ed3ac702468b5fe766502f5880
Plamena <yomena37@gmail.com>
2022-05-15 16:27:34 +0300

    Implement sorting

--- script.js
+++ script.js
@@ -67,11 +67,13 @@ const inputClosePin = document.querySelector('.form__input--pin');
 //*************************************************************************************************
 // Display Movements
 //*************************************************************************************************
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   // Emptying the movements container
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -236,4 +238,15 @@ btnClose.addEventListener('click', function (event) {
   }
 
   inputCloseUsername.value = inputClosePin.value = '';
+});
+
+//*************************************************************************************************
+// Implement Sorting
+//*************************************************************************************************
+let sorted = false;
+btnSort.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
